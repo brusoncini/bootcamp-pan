b6af255022c9070e565df16a8d1eccb820ff23f8
Bruna Soncini <brusoncini@gmail.com>
2023-04-18 22:36:58 -0300

    Primeiro commit

---
+++ src/desafios/de/codigo/FabricaDeCarros.java
@@ -0,0 +1,20 @@
+package desafios.de.codigo;
+
+import java.util.Scanner;
+
+public class FabricaDeCarros {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int custoFabrica = scan.nextInt();
+        int porcentagemDistribuidor = scan.nextInt();
+        int percentualImpostos = scan.nextInt();
+
+        // TODO: Implemente uma condição que calcule a porcentagem do distribuidor e dos impostos:
+        int distribuidor = (custoFabrica * porcentagemDistribuidor) / 100;
+        int valorImpostos = (custoFabrica * percentualImpostos) / 100;
+        int custoConsumidor = (custoFabrica + distribuidor + valorImpostos);
+
+        System.out.println(custoConsumidor);
+    }
+}
